ed1b1d08993db0db67f306e25f8ef059d965cc37
karanumashankarpandey <161342445+karanumashankarpandey@users.noreply.github.com>
2026-05-12 16:16:00 +0000

    datatypes

---
+++ basics/02_DataTypes.js
@@ -0,0 +1,37 @@
+"use strict ";
+// it 
+
+//alert(3+3) // so we are using node js not the browser thats why it will not work. 
+console.log(3+3);
+
+console.log("hello karan");
+
+
+// DATATYPES
+let name = "Karan" // it is string datatype.
+let age = 18 // my current age hehe it is integer datatype.
+let isLoggedin = true // it is the boolean type it also has flase in it.
+
+
+// there are multiple datatypes
+/* 
+number => integer values.
+bigint => large number of integer values.
+String => ""
+boolean => true or false
+null => standalone value
+undefined => it is written in the code but it is not defined foreg => let string;
+symbol => it is unique.
+
+*/
+
+// object DataTypes.
+
+// typeof is used to print the type of the variable
+console.log(typeof "hello Karan") // output is string
+console.log(typeof null) // the output is object.
+console.log(typeof object) // the output is undefined
+console.log(typeof age); // o/p is number
+console.log(typeof undefined); // the output is undefined
+
+
