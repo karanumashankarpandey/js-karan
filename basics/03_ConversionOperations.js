dbda90322f8aaed5b5893b32261f8e00868d4b85
karanumashankarpandey <161342445+karanumashankarpandey@users.noreply.github.com>
2026-05-13 17:56:11 +0000

    conversion of datatypes

---
+++ basics/03_ConversionOperations.js
@@ -0,0 +1,30 @@
+let score = "33abc" // the number is in string 
+//console.log(typeof score);
+
+let valueInNumber = Number(score); // it is used to convert string number into number.
+//console.log(valueInNumber);
+//console.log(typeof (valueInNumber));
+
+// "33" => 33
+//"33abc"=> NaN   it is called not a number
+// null => 
+// true => 1
+// false => 0
+
+
+let isLoggedIn = " karan "
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1=> true , 0=> false
+// "" => false 
+//  " " => true 
+//  " karan "=> true.
+
+let stringSomeNumber = 33
+
+let stringNumber = String(stringSomeNumber);
+console.log(typeof(stringNumber));
+
+
+
