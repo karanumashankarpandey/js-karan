f4e3af96c06414ffcb6d6368d739b5e5037ddc25
karanumashankarpandey <karankp2505@gmail.com>
2026-05-14 23:22:36 +0530

    hello

---
+++ basics/04_ComparisonofDatatypes.js
@@ -0,0 +1,16 @@
+// console.log("hello"+1>2);
+// console.log(1>=2);
+// console.log(1<2);
+// console.log(1<=2);
+// console.log(1!=2);
+
+console.log("2">1); // even if the 2 is in string then also js converts string into the number.
+
+console.log(null>0); //flase
+console.log(null<0);//false
+console.log(null>=0);//true
+console.log(null<=0);//true
+
+
+
+
