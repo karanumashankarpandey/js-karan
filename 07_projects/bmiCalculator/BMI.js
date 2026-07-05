9a8632b44de34713ec7e67738e97b6d425b10083
karanumashankarpandey <karankp2505@gmail.com>
2026-07-05 21:12:01 +0530

    bmi

---
+++ 07_projects/bmiCalculator/BMI.js
@@ -0,0 +1,45 @@
+const form=document.querySelector('form')
+
+form.addEventListener('submit', function (event){
+    event.preventDefault()                // prevent default , default action ko rokne ke liye use kiya jata hai
+                                        // such as giving the data to the website directly 
+    
+     const height = parseInt (document.querySelector('#Height').value)   // .value gives the user value , which is in string
+                                                                            // parseint used to convert string into integer value
+    
+     const weight = parseInt (document.querySelector('#Weight').value)
+
+     const result = document.querySelector('#result')
+
+     if(height===' ' || height<0 || isNaN(height)){
+        result.innerHTML= ` please give a valid height `
+     }
+
+    else if(weight===' ' || weight<0 || isNaN(weight) ){
+        result.innerHTML= ` please give a valid weight `
+     }
+
+     else{
+        const bmi = (weight / ((height*height)/10000)).toFixed(2)
+        // now showing the result
+         result.innerHTML= `<span> The result is = ${bmi} </span>`
+        
+          if (bmi<18.6) {
+        result.innerHTML=`${bmi} is under weight`
+     }
+         else if (bmi<24.9) {
+        result.innerHTML=`${bmi} is normal  weight`
+     }
+        else  if (bmi>24.9) {
+        result.innerHTML=`${bmi} is over weight`
+     }
+      
+
+     }
+
+   
+
+
+
+} )
+
